20e105d9d43dbfd3bd9b38a7365470c17b82de2d
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-09-24 22:41:28 +0300

    add scaling text and animation on splash view body

---
+++ lib/features/splash/presentation/views/widgets/scaling_text.dart
@@ -0,0 +1,28 @@
+import 'package:bookly/core/utils/helper.dart';
+import 'package:flutter/material.dart';
+class ScalingSplashText extends StatelessWidget {
+  const ScalingSplashText({
+    super.key,
+    required this.scalingAnimation,
+  });
+  final Animation<double> scalingAnimation;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: scalingAnimation,
+      builder: (context, _) {
+        return FadeTransition(
+          opacity: scalingAnimation,
+          child: Text(
+            textAlign: TextAlign.center,
+            'read free books'.capitalizeEachWord(),
+            style: const TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,27 +1,52 @@
+import 'dart:developer';
 import 'package:bookly/core/utils/assets.dart';
-import 'package:bookly/core/utils/helper.dart';
 import 'package:flutter/material.dart';
+import 'scaling_text.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scalingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _scalingAnimation = Tween<double>(begin: 0.4, end: 1.2).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+    _animationController.forward();
+    _animationController.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    log('Build Screen');
     return Column(
-      spacing: 4,
+      spacing: 16,
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(width: 200, height: 50, Assets.logo),
-
-        Text(
-          textAlign: TextAlign.center,
-          'read free books'.capitalizeEachWord(),
-          style: const TextStyle(
-            fontSize: 28,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        ScalingSplashText(scalingAnimation: _scalingAnimation),
       ],
     );
   }
