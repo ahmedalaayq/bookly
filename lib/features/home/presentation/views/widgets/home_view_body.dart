c5c67cb745146c3734decacf0755a9921e0b4bc3
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-09-25 23:17:16 +0300

    best seller list view item part 1

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,9 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/helper.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'custom_feature_books_list_view.dart';
+
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
@@ -14,10 +16,44 @@ class HomeViewBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomFeatureBooksListView(mediaQuery: mediaQuery),
-          Text('best seller'.capitalizeEachWord(),style: Styles.titleMedium,),
+          Text(
+            'best seller'.capitalizeEachWord(),
+            style: Styles.titleMedium,
+          ),
+          const SizedBox(height: 16),
+          const BestSellerListViewItem(),
         ],
       ),
     );
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+        AspectRatio(
+      aspectRatio: 2.5 / 4,
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(Assets.testImage2),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    ),
+          const Column(children: [
+      
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
