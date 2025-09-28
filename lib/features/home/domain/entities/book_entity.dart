df1408f7e1c45cd2cae73dbec004fa552f6101a6
ahmed alaayq <ahmed.alaayq@gmail.com>
2025-09-28 12:24:13 +0300

    apply domain layer on project and create book entity that represent general data on home feature

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,13 @@
+class BookEntity {
+  final String title;
+  final String author;
+  final num price;
+  final num rating;
+
+  const BookEntity({
+    required this.title,
+    required this.author,
+    required this.price,
+    required this.rating,
+  });
+}
